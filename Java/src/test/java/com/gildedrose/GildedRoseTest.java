3677a783ed4ec4c935ce6e73b77fb8785028263c
Lalith Sai Swaroop <lalith.swaroop@teampicnic.com>
2022-07-21 16:02:25 +0200

    Implement unnamed items test case

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void conjuredManaCakeTest() {
-        Item[] items = new Item[] { new Item(ItemNames.CONJURED_MANA_CAKE, 0, 1) };
+    void unnamedItemsTest() {
+        Item[] items = new Item[] {
+            new Item(ItemNames.ELIXIR_OF_THE_MONGOOSE, 0, 1),
+            new Item(ItemNames.CONJURED_MANA_CAKE, 0, 1),
+            new Item(ItemNames.DEXTERITY_VEST, 0, 1)};
+
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(app.items[0].quality, -1);
-        assertEquals(app.items[0].sellIn, 0);
+        for (int i = 0; i < items.length; i++) {
+            assertEquals(app.items[i].quality, -1);
+            assertEquals(app.items[i].sellIn, 0);
+        }
     }
+
 }
